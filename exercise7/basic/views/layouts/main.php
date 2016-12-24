a4a9ba589bffe80340d6ae8222ef07245dbdd757
stgallangi <stevegallangi@gmail.com>
2016-12-24 19:52:00 +0800

    minor changes

--- exercise7/basic/views/layouts/main.php
+++ exercise7/basic/views/layouts/main.php
@@ -27,7 +27,7 @@ AppAsset::register($this);
 <div class="wrap">
     <?php
     NavBar::begin([
-        'brandLabel' => 'Stevenson Gallangi',
+        'brandLabel' => 'Gallangi, Stevenson',
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',
